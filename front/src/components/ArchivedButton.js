1f36062656f09f45127c792454a9c577dcc9fa19
Anton <60780638+antonio63963@users.noreply.github.com>
2024-09-16 12:41:17 +0300

    archived button

---
+++ front/src/components/ArchivedButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Stack, Button, Divider } from "@mui/material";
+import { ArchiveBox } from "phosphor-react";
+
+function ArchivedButton({ onClick }) {
+  return (
+    <Stack spacing={1} sx={{ mt: 2 }}>
+      <Stack direction="row" alignItems="center" spacing={1.5}>
+        <ArchiveBox size={24} />
+        <Button onClick={onClick}>Archived</Button>
+      </Stack>
+      <Divider sx={{ height: 1, width: "100%" }} />
+    </Stack>
+  );
+}
+
+export default ArchivedButton;
